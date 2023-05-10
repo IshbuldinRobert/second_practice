5bdd0b2cc707f0e1fd8c64196151caf133b9a81e
Robert <ishbuldinrobert2002@gmail.com>
2023-05-10 14:50:17 +0500

    add example with cycle for

---
+++ second_practice/example1/Program.cs
@@ -0,0 +1,6 @@
+﻿for (int num = 2; num <= 10; num += 2)
+{
+    Console.WriteLine($"{num} c");
+}
+// Console.WriteLine($"{num} OK"); - это неправильно
+// переменная num существует только в теле цикла for
