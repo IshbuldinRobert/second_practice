33a583d539b9eef4442e85d025414913719a2a8f
Robert <ishbuldinrobert2002@gmail.com>
2023-05-10 17:45:23 +0500

    add exr16

---
+++ second_practice/exr16/Program.cs
@@ -0,0 +1,10 @@
+﻿// программа принимает на вход два числа и проверяет, является ли одно число квадратом другого
+// например: 5, 25 -> да; 25, 5 -> да; 8, 9 -> нет
+
+int c = Convert.ToInt32(Console.ReadLine());
+int s = Convert.ToInt32(Console.ReadLine());
+
+if ((c / s == s) || (s / c == c))
+    Console.WriteLine($"{c}, {s} -> да");
+else
+    Console.WriteLine($"{c}, {s} -> нет");
